d01ca2d29ed4ae3317137a848ff72004d9302016
Deepak Karuppiah <dkaruppiah@microstrategy>
2022-10-19 22:27:55 -0400

    Simplify interfaces further

--- sample.py
+++ sample.py
@@ -1,22 +1,21 @@
 from collections.abc import Iterable
+from typing import List, Dict
 
 from obaic.model import ObaicModel
-from obaic.requests import ObaicModelQuery, PredictionRequest
-from obaic.responses import ObaicOutput
-from obaic.source import MLSource
-from obaic.sourcedefinitions import MLSourceDefinition
+from obaic.source import MLSource, ObaicFeature, ObaicOutput
 
 def main():
 
-    # define an ML source where predictions will be done
-    ml_src_defn: MLSourceDefinition = MLSourceDefinition()
+    ml_src_parameters: Dict[str, str]
+    ml_src_credentials: Dict[str, str]
 
-    # connect to the ML source
-    ml_src: MLSource = MLSource(ml_src_defn)
+    # define an ML source where predictions will be done
+    ml_src: MLSource = MLSource(ml_src_parameters=ml_src_parameters,
+                                ml_src_credentials=ml_src_credentials)
 
     # 1. define a model query and fetch a list of models from the ML source
-    model_query: ObaicModelQuery = ObaicModelQuery()
-    ml_model_iter: Iterable[ObaicModel] = ml_src.list_models(model_query)
+    model_query: str
+    ml_model_iter: Iterable[ObaicModel] = ml_src.list_models(model_query=model_query)
 
     # 3. pick a model from the list mlModels using some criteria
     for ml_model in ml_model_iter:
@@ -24,6 +23,6 @@ def main():
             select_ml_model = ml_model
             break
 
+    features: List[ObaicFeature] 
     # 4. define a prediction request and run a prediction using the model selected above
-    prediction_req: PredictionRequest = PredictionRequest()
-    prediction_response: list[ObaicOutput] = ml_src.predict(select_ml_model, prediction_req)
+    prediction_response: List[ObaicOutput] = ml_src.predict_with_features(model=select_ml_model, features=features)
--- sample_async.py
+++ sample_async.py
@@ -2,31 +2,37 @@ from collections.abc import Iterable
 from mailcap import findmatch
 from obaic import obaic
 from obaic.model import ObaicModel
-from obaic.requests import ObaicModelQuery, PredictionRequest
-from obaic.source import MLSource
-from obaic.sourcedefinitions import MLSourceDefinition
+from obaic.source import MLSource, ObaicFeature, ObaicOutput
 
 import asyncio
 
 async def main():
 
-    # define an ML source where predictions will be done
-    ml_src_defn: MLSourceDefinition = MLSourceDefinition()
+    ml_src_parameters: dict[str, str]
+    ml_src_credentials: dict[str, str]
 
-    # connect to the ML source
-    ml_src: MLSource = MLSource(ml_src_defn)
+    # define an ML source where predictions will be done
+    ml_src: MLSource = MLSource(ml_src_parameters=ml_src_parameters, 
+                                ml_src_credentials=ml_src_credentials)
 
     # 1. define a model query and fetch a list of models from the ML source
-    model_query: ObaicModelQuery = ObaicModelQuery()
-    ml_model_iter = ml_src.list_models_async(model_query)
+    model_query: str
+    ml_model_iter: Iterable[ObaicModel] = ml_src.list_models_async(model_query=model_query)
 
     # 3. pick a model from the list mlModels using some criteria
-    async for ml_model in ml_model_iter:
+    for ml_model in ml_model_iter:
         if ml_model : # check for some criteria
             select_ml_model = ml_model
             break
 
     # 4. define a prediction request and run a prediction using the model selected above
-    prediction_req: PredictionRequest = PredictionRequest()
-    predict_task = asyncio.create_task(ml_src.predict(select_ml_model, prediction_req))
+    data_src_parameters: dict[str,str]
+    data_src_credentials: dict[str,str]
+    data_query: str
+    predict_task = asyncio.create_task(
+                        ml_src.predict_with_data_ref_async(model=select_ml_model, 
+                                                           data_src_parameters=data_src_parameters,
+                                                           data_src_credentials=data_src_credentials,
+                                                           data_query=data_query)
+                    )
     await predict_task
